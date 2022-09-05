0d13abd970cb4826034bdb9fb23a4c5c9700e44d
R.Gokulprasath <79097212+Gokulprasath2002@users.noreply.github.com>
2022-09-05 21:47:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,url_for,redirect,request
+import numpy as np
+import pandas as pd
+import pickle
+import bz2
+app = Flask(__name__)  
+def decompress_pickle(file):
+
+    data = bz2.BZ2File(file, 'rb')
+    data = pickle.load(data)
+    return data
+model = decompress_pickle('./venv/filename.pbz2')
+@app.route('/')
+def home():  
+    return render_template("index.html");  
+    
+@app.route('/checking',methods=['POST'])
+def checking():
+    
+    data=np.array(list(request.form.values()))
+    values=model.predict([[data[1],data[2],data[3],data[4],data[5]]])
+    if values == 0 :
+        result="Don't worry, You don't have Insomnia"
+        flag=0
+    else:
+        result="We are sorry but you have Insomnia"
+        flag=1
+    if data[7]=='yes'or 'YES' or 'Yes' or '1':
+        mp=1
+    else:
+        mp=0
+    return render_template('result.html',result=result,flag=flag,name=data[0],age=data[1],sleep=data[6],mp=mp,daysleep=data[8],drowsy=data[9])
+if __name__ =='__main__':  
+    app.run()  
