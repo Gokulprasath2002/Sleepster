b8b0b38c001c847bc6e2256bfb0e0ca2d2de50bc
R.Gokulprasath <79097212+Gokulprasath2002@users.noreply.github.com>
2022-12-20 21:20:54 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,12 @@
 import numpy as np
+import joblib
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-import pickle
-import bz2
-data=pd.read_excel("Sleepsterdataset.xlsx")
+url ='https://sleepster.blob.core.windows.net/newcontainer/Sleepsterdataset.xlsx?sp=r&st=2022-12-20T14:49:25Z&se=2022-12-20T22:49:25Z&spr=https&sv=2021-06-08&sr=b&sig=nZOiY3NqMQq9CoVPoXmoASfxMY5iuNWahD%2FKarVL8jw%3D'
+df = pd.read_excel(url)
 data['age']=data['age'].apply(np.ceil).astype(int)
 data['weight']=data['weight'].apply(np.ceil).astype(int)
 x=data.iloc[:,0:5]
 y=data.iloc[:,-1]
 model=RandomForestClassifier()
 model.fit(x,y)
-def compressed_pickle(title, data):
-
-    with bz2.BZ2File(title + '.pbz2', 'w') as f:
-        pickle.dump(data, f)
-compressed_pickle('filename', model)
+joblib.dump(model, "model.joblib", compress=3)
